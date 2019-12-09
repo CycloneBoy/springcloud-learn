4e5d9a7c18c8b9696de130a15927efbfbd100939
cycloneboy <xuanfeng1992@gmail.com>
2019-12-09 21:50:07 +0800

    添加秒杀业务相关的代码,利于学习分布式锁

---
+++ goods-kill/src/main/java/com/cycloneboy/springcloud/goodskill/GoodsKillApplication.java
@@ -0,0 +1,13 @@
+package com.cycloneboy.springcloud.goodskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsKillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsKillApplication.class, args);
+	}
+
+}
---
+++ goods-kill/src/test/java/com/cycloneboy/springcloud/goodskill/GoodsKillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cycloneboy.springcloud.goodskill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsKillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
